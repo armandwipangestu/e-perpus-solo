cba0b94407a1e3c5570d92303c3ea0834b8dde42
Arman <armandwi.pangestu7@gmail.com>
2023-12-11 21:29:24 +0700

    update: admin dashboard

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -31,12 +31,12 @@ class Admin extends CI_Controller
         $data['user'] = $this->db->get_where('user_data', ['email' => $this->session->userdata('email')])->row_array();
         $data['total_account'] = $this->db->from('user_data')->count_all_results();
         $data['total_admin'] = $this->admin->getTotalByRole("Administrator")['total'];
+        $data['total_operator'] = $this->admin->getTotalByRole("Operator")['total'];
         $data['total_user'] = $this->admin->getTotalByRole("User")['total'];
         $data['total_role'] = $this->db->from('user_role')->count_all_results();
         $data['total_publisher'] = $this->db->from('book_publisher')->count_all_results();
         $data['total_author'] = $this->db->from('book_author')->count_all_results();
         $data['total_book'] = $this->db->from('book_data')->count_all_results();
-        $data['total_log'] = $this->db->from('user_log_action')->count_all_results();
         $data['user_registration'] = json_encode($this->admin->getUserRegistration());
         $data['book_registration'] = json_encode($this->admin->getBookRegistration());
         $data['recent_users'] = $this->admin->getRecentUsers();
--- application/views/admin/admin_index.php
+++ application/views/admin/admin_index.php
@@ -55,6 +55,23 @@
                             <div class="row">
                                 <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
                                     <div class="stats-icon green mb-2">
+                                        <i class="iconly-boldWork"></i>
+                                    </div>
+                                </div>
+                                <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
+                                    <h6 class="text-muted font-semibold">Total Operator</h6>
+                                    <h6 class="font-extrabold mb-0"><?= $total_operator; ?></h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-lg-3 col-md-6">
+                    <div class="card">
+                        <div class="card-body px-4 py-4-5">
+                            <div class="row">
+                                <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
+                                    <div class="stats-icon red mb-2">
                                         <i class="iconly-boldAdd-User"></i>
                                     </div>
                                 </div>
@@ -66,6 +83,9 @@
                         </div>
                     </div>
                 </div>
+            </div>
+
+            <div class="row">
                 <div class="col-6 col-lg-3 col-md-6">
                     <div class="card">
                         <div class="card-body px-4 py-4-5">
@@ -83,9 +103,6 @@
                         </div>
                     </div>
                 </div>
-            </div>
-
-            <div class="row">
                 <div class="col-6 col-lg-3 col-md-6">
                     <div class="card">
                         <div class="card-body px-4 py-4-5">
@@ -132,23 +149,6 @@
                                 <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
                                     <h6 class="text-muted font-semibold">Total Book</h6>
                                     <h6 class="font-extrabold mb-0"><?= $total_book; ?></h6>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-6 col-lg-3 col-md-6">
-                    <div class="card">
-                        <div class="card-body px-4 py-4-5">
-                            <div class="row">
-                                <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start">
-                                    <div class="stats-icon red mb-2">
-                                        <i class="bi bi-book d-flex justify-content-center pb-4"></i>
-                                    </div>
-                                </div>
-                                <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
-                                    <h6 class="text-muted font-semibold">Total Log</h6>
-                                    <h6 class="font-extrabold mb-0"><?= $total_log; ?></h6>
                                 </div>
                             </div>
                         </div>
